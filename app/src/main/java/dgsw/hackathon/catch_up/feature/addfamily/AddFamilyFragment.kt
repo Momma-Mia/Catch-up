3f940aa0e73fa638bcd647f87bf86beabad38d95
wyk172899 <wyk172899@gmail.com>
2023-07-12 17:20:42 +0900

    MODIFY :: Adapter

--- app/src/main/java/dgsw/hackathon/catch_up/feature/addfamily/AddFamilyFragment.kt
+++ app/src/main/java/dgsw/hackathon/catch_up/feature/addfamily/AddFamilyFragment.kt
@@ -5,18 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import dgsw.hackathon.catch_up.R
+import dgsw.hackathon.catch_up.databinding.FragmentAddFamilyBinding
+import dgsw.hackathon.catch_up.model.User
 
 class AddFamilyFragment : Fragment() {
+    private val binding by lazy { FragmentAddFamilyBinding.inflate(layoutInflater) }
+    private val viewModel: AddFamilyViewmodel by activityViewModels()
+    private lateinit var userList: ArrayList<User>
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        
     }
 }
