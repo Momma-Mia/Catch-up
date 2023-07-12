536be5897b7e80a491b5ad8c1229bcd0c2adfe80
wyk172899 <wyk172899@gmail.com>
2023-07-12 10:31:27 +0900

    package separation
    제곧내

--- app/src/main/java/dgsw/hackathon/catch_up/feature/intro/IntroActivity.kt
+++ app/src/main/java/dgsw/hackathon/catch_up/feature/intro/IntroActivity.kt
@@ -1,11 +1,12 @@
-package dgsw.hackathon.catch_up
+package dgsw.hackathon.catch_up.feature.intro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dgsw.hackathon.catch_up.R
 
-class MainActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_intro)
     }
 }
---
+++ app/src/main/java/dgsw/hackathon/catch_up/feature/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package dgsw.hackathon.catch_up.feature.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dgsw.hackathon.catch_up.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
